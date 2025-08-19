7425313d658dd422a14683c388ff41f098afab0e
Dmytro <dmytrokomissarchik@gmail.com>
2025-08-19 18:31:02 +0300

    added model event

---
+++ events/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=100)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
