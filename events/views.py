27f8bf53be1c3369b9c0dd4298b695cca75258a6
Dmytro <dmytrokomissarchik@gmail.com>
2025-08-21 14:40:55 +0300

    added filters

--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets, permissions
-from .models import Event
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Event, EventRegistration
 from .serializers import EventSerializer, EventRegistrationSerializer
 
 
@@ -7,6 +8,20 @@ class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['title', 'description', 'location']
+    search_fields = ['title', 'description', 'location']
+    ordering_fields = ['date']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        registered = self.request.query_params.get('registered')
+        if registered == 'true':
+            user = self.request.user
+            registered_events = EventRegistration.objects.filter(user=user).values_list('event_id', flat=True)
+            queryset = queryset.filter(id__in=registered_events)
+        return queryset
 
 
 class EventRegistrationViewSet(viewsets.ModelViewSet):
